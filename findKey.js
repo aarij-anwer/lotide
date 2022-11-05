c80e50c13ab84bc9baaed8cf1c93621e24125de3
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-05 14:01:40 -0400

    findKey

---
+++ findKey.js
@@ -0,0 +1,50 @@
+const findKey = function(object, callback) {
+  let keys = Object.keys(object);
+
+  for (let i = 0; i < keys.length; i++) {
+    if (callback(object[keys[i]])) {
+      return keys[i];
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(test1, "noma");
+
+const test2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3); // => "Akaleri"
+
+assertEqual(test2, "Akaleri");
+
+const test3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars > 3); // => undefined
+
+assertEqual(test3, undefined);
