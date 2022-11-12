01d04f6a1027268771d587375a2129d429514f74
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-12 11:37:06 -0500

    eqArrays

--- test/headTest.js
+++ test/headTest.js
@@ -3,5 +3,5 @@ const head = require('../head');
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+assertEqual(head([]), undefined);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
